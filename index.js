e4fa63db6313c67530514cb78080dd85d12aec1d
Hammad <mohammedhammad07@gmail.com>
2022-10-06 14:11:57 +0300

    dynamic color change of card

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {uuidv4} from "./uuid.js";
+
 const $ = (x) => {
     return document.getElementById(x);
 }
@@ -7,10 +9,25 @@ const $create = (x) => {
 }
 
 const todoCol = $("todo");
-const doingCol = $("doing");
-const doneCol = $("done");
+todoCol.addEventListener('drop', drop);
+todoCol.addEventListener('dragover', allowDrop);
 
-function form(){
+
+const doingCol = $("doing");
+doingCol.addEventListener('drop', drop);
+doingCol.addEventListener('dragover', allowDrop);
+
+const doneCol = $("done");
+doneCol.addEventListener('drop', drop);
+doneCol.addEventListener('dragover', allowDrop);
+
+
+
+
+
+
+
+function inputHandler(){
   const inputBox = $("inputBox");
   const form = $("form1");
   let task = "";
@@ -32,31 +49,71 @@ function form(){
 }
 
 
-function createCard(text){
-    const card = $create("div");
-    card.classList.add('card', 'mt-2');
-    card.style.width = "auto";
 
+function createCard(text){
     const cardBody = $create("div");
-    cardBody.classList.add('card-body', 'bg-info' ,'bg-opacity-25');
+    cardBody.classList.add('card', 'card-body', 'bg-info' ,'bg-opacity-25', 'mt-2');
+    cardBody.style.cursor = "move";
+    cardBody.setAttribute('draggable', 'true');
+    cardBody.setAttribute('id', uuidv4());
+    cardBody.addEventListener('dragstart', drag);
+
+    cardBody.addEventListener('dragend', (ev)=>{
+      ev.target.classList.remove('border', 'border-dark');
+
+      if (ev.currentTarget.parentNode.id === 'todo'){
+        ev.target.classList.add('bg-info');
+        ev.target.classList.remove('bg-warning');
+        ev.target.classList.remove('bg-success');
+        console.log(ev.target.classList);
+      }
+      if (ev.currentTarget.parentNode.id === 'doing'){
+        ev.target.classList.add('bg-warning');
+        ev.target.classList.remove('bg-info');
+        ev.target.classList.remove('bg-success');
+        console.log(ev.currentTarget);
+      }
+      if (ev.currentTarget.parentNode.id === 'done'){
+        ev.target.classList.add('bg-success');
+        ev.target.classList.remove('bg-info');
+        ev.target.classList.remove('bg-warning');
+        console.log(ev.target.classList);
+      }
+    })
 
     const cardTitle = $create("h3");
     cardTitle.className = "card-title";
     cardTitle.textContent = text;
 
-    const button = $create("button");
-    button.classList.add('btn', 'btn-primary', 'btn-sm');
-    button.textContent = "Move";
-
     cardBody.appendChild(cardTitle);
-    cardBody.appendChild(button);
-    card.appendChild(cardBody);
+  
+    return cardBody;
+}
 
-    return card;
+function allowDrop(ev) {
+  if (ev.target.id === 'todo' || ev.target.id  === 'doing' || ev.target.id === 'done'){
+    ev.stopPropagation();
+    ev.preventDefault();
+  }
+}
+
+function drag(ev) {
+  ev.stopPropagation();
+  ev.target.classList.add('border', 'border-dark');
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.stopPropagation();
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
 }
 
 
-form();
+
+
+inputHandler();
 
 
 
